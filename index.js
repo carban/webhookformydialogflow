7526ba661960c87ab670dfc5c5419d33fb30e8d4
carban <murillo.carlos@correounivalle.edu.co>
2020-05-08 11:44:32 -0500

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+//imports
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+//config
+app.set('port', process.env.PORT || 8000);
+
+//middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(express.json());///Hace lo mismo del bodyParser
+app.use(morgan('dev'));
+app.use(cors());
+
+//routes
+app.post("/", (req, res) => {
+  res.json({ hola: true });
+})
+//start server
+app.listen(app.get('port'), () => {
+  console.log('😬 Backend Running on port:', app.get('port'));
+})
